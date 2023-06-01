724163223633d875ef65f5cee5118d5bee187a6e
AudioBF <audiobf@gmail.com>
2023-06-01 11:04:30 -0300

    adicionado arquivos

---
+++ string.js
@@ -0,0 +1,5 @@
+//               01234567
+let umaString = `um texto`;
+
+console.log(umaString.indexOf('o'));
+// para substituir uma letra é (umaString.replace(/letra/, 'subistuto')); para todas as letras basta adicionar o g apos a /, /letra/g
